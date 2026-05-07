71aeebb2b19850e284f7a5501652ebea6468d9b9
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-07 06:39:56 +0500

    update

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,6 +1,9 @@
 // routes/auth.routes.js
 import { getMe } from '../controllers/authController.js'; 
 import { authenticateUser } from '../middleware/authMiddleware.js';
+import router from './authRoutes.js';
 
 // This is the endpoint the frontend calls on App.js/App.tsx mount
 router.get('/me', authenticateUser, getMe);
+
+export default router
