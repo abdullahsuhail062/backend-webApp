ce7348ee0a04ab84d733a4309ddb913378383598
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2026-05-18 07:55:54 +0500

    update

--- src/utils/auth.service.js
+++ src/utils/auth.service.js
@@ -44,7 +44,6 @@ export const clearAuthCookies = (res) => {
 };
 
 export const setAuthCookiesForAccessToken = (res, tokens) => {
-  const isProduction = process.env.NODE_ENV === 'production';
 
   const accessTokenOptions = {
     httpOnly: true,
